111f10bef51c2c6227c303a9819c19d353f08889
耗子 <HendrixShen@qq.com>
2020-12-28 23:54:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MiniHUD-extra
---
+++ src/main/java/PhoeniX/MiniHUDExtra/MiniHUDExtra.java
@@ -0,0 +1,10 @@
+package PhoeniX.MiniHUDExtra;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MiniHUDExtra implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Masa增强: MiniHUD 模块已加载!");
+    }
+}
