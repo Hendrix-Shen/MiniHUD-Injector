e8db428857c6f6fca2c756277d1ae4a459dc9164
Hendrix-Shen <HendrixShen@qq.com>
2021-07-27 23:07:15 +0800

    Rewrite

---
+++ src/main/java/top/hendrixshen/MiniHUDInjector/Reference.java
@@ -0,0 +1,8 @@
+package top.hendrixshen.MiniHUDInjector;
+
+public class Reference {
+    public static final String MOD_ID = "minihud-injector";
+    public static final String MOD_NAME = "MiniHUD-Injector";
+    public static final String MOD_VERSION = "1.1.0-build.undefined";
+    public static final String MOD_VERSION_TYPE = "Version Exception";
+}
---
+++ src/main/java/top/hendrixshen/MiniHUDInjector/mixin/RenderHandlerMixin.java
@@ -0,0 +1,20 @@
+package top.hendrixshen.MiniHUDInjector.mixin;
+
+import fi.dy.masa.minihud.event.RenderHandler;
+import net.minecraft.client.resource.language.I18n;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(value = RenderHandler.class, remap = false)
+public class RenderHandlerMixin {
+    @Redirect(
+            method = "addLine(Lfi/dy/masa/minihud/config/InfoToggle;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"
+            )
+    )
+    private String onStringFormat(String format, Object[] args) {
+        return I18n.translate(format, args);
+    }
+}
