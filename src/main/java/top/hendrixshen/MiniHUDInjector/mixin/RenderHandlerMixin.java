a3bdbc7d650e5a26143857e814caba8ace34d5ac
Hendrix-Shen <HendrixShen@qq.com>
2021-07-31 09:13:31 +0800

    Support for more strings using I18N

--- src/main/java/top/hendrixshen/MiniHUDInjector/mixin/RenderHandlerMixin.java
+++ src/main/java/top/hendrixshen/MiniHUDInjector/mixin/RenderHandlerMixin.java
@@ -3,11 +3,19 @@ package top.hendrixshen.MiniHUDInjector.mixin;
 import fi.dy.masa.minihud.event.RenderHandler;
 import net.minecraft.client.resource.language.I18n;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import top.hendrixshen.MiniHUDInjector.MiniHUDInjector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Mixin(value = RenderHandler.class, remap = false)
 public class RenderHandlerMixin {
+    private static final Pattern TOKEN_PATTERN = Pattern.compile("[\\w]+: ");
     @Redirect(
             method = "addLine(Lfi/dy/masa/minihud/config/InfoToggle;)V",
             at = @At(
@@ -17,5 +25,26 @@ public class RenderHandlerMixin {
     )
     private String onStringFormat(String format, Object[] args) {
         return I18n.translate(format, args);
+    }
+
+    @ModifyVariable(
+            method = "addLine(Ljava/lang/String;)V",
+            at = @At(
+                    value = "HEAD"
+            )
+    )
+    private String onAddLine(String string) {
+        Matcher matcher = TOKEN_PATTERN.matcher(string);
+        int start = 0;
+        List<String> keys = new ArrayList<>();
+        while (matcher.find(start)) {
+            keys.add(matcher.group());
+            start = matcher.end();
+        }
+        for (String key: keys) {
+            string = string.replace(key, I18n.translate(key));
+        }
+        MiniHUDInjector.logger.info(string);
+        return string;
     }
 }
